ab4d5c0602b1c63b7f6a5e919661487c1ae8c361
Tiago Salmito <tiagobraw@gmail.com>
2013-02-04 22:45:50 -0200

    Fixed critical bug that destroys all lighuserdatum in events

--- src/leda/kernel/event.c
+++ src/leda/kernel/event.c
@@ -84,7 +84,7 @@ void destroy_event(event e) {
          case LUA_TFUNCTION:
          case LUA_TTABLE:
          case LUA_TUSERDATA:
-			case LUA_TLIGHTUSERDATA:
+		//	case LUA_TLIGHTUSERDATA:
          if(e->payload[i].data.str) 
             free(e->payload[i].data.str);
          }
