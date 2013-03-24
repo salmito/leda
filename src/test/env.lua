c562578afd159f6e2e538dbe3deca93cb182f7df
Tiago Salmito <tiago@salmito.com>
2013-03-23 21:24:39 -0300

    Removed build scripts

---
+++ src/test/env.lua
@@ -0,0 +1,13 @@
+local utils=require "leda.utils"
+
+local n=10
+
+printer=leda.stage(function(i) j=(j or 0)+1 print(i,j) if i==n then leda.quit() end end)
+
+local g=leda.graph{start=printer}
+
+for i=1,n do
+   printer:send(i)
+end
+
+g:run{maxpar=2}
