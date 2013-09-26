a38ac07343fdde7a236c89fe953137c39af8c2ee
Tiago Salmito <tiago@salmito.com>
2013-09-26 00:25:24 -0300

    small fix

--- examples/file_read.lua
+++ examples/file_read.lua
@@ -5,35 +5,46 @@ local io=nil
 
 s0=leda.stage{
    handler=function(file)
-      async=true
       local io=require'io'
       print("Opening file",file)
       local f = io.open(file,"r")
       leda.send("file",f)
    end,
-	init=function() async=true end
 }
 
 s1=leda.stage{
    handler=function(file)
       print("Reading file",file)
       local buffer,err = assert(file:aread(4096))
+      local bytes=0
       while buffer do
+	      bytes=bytes+#buffer
    	   leda.send("line",buffer)
 	      buffer,err = file:aread(4096)
       end
-		leda.send("EOF")
+		leda.send("EOF",bytes)
    end,
-	init=function() async=true require "io" end
+--	init=function() async=true end
 }
 
 s2=leda.stage{
 	handler=function (str) print(str) end,
---	serial=true
 }
 
-g=leda.graph{leda.connect(s1,'line',s2),leda.connect(s0,'file',s1)}
+sQuit=leda.stage{
+	handler=function (...) leda.quit(...) end,
+}
+
+--three ways to connect two stages:
+g=leda.graph{
+	--leda.connect function 
+	leda.connect(s0,'file',s1),
+	--stage object metamethod
+	s1:connect('line',s2),
+	--stage concat operator
+	s1'EOF'..sQuit
+}
 
 s0:send(assert(arg[1]))
 
-g:run()
+print(g:run())
